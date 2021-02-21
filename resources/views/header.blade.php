dcc42c2bb82f9e84aadff6386b2d468f0868c614
David-Karanja <69545783+David-Karanja@users.noreply.github.com>
2021-02-21 16:40:04 +0300

    header, footer and login page done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+  
+  </head>
+  <body>
+
+
+
+<div class="navbar navbar-expand-md navbar-dark bg-dark mb-4" role="navigation">
+    <a class="navbar-brand" href="#">Bootstrap 4 NavBar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Orders</a></li>
+
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Cart(0)</a></li>
+        </ul>
+    </div>
+</div>
+
+<!--  <main role="main" class="container">
+    <div class="jumbotron text-center">
+        <h1>Bootstrap 4 NavBar</h1>
+        <p class="lead text-info">NavBar with too many childs.</p>
+        <a href="http://fontenele.github.io/bootstrap-navbar-dropdowns/">Github</a>
+    </div>
+</main>
+
+-->
+
+
+  
+</body>
+</html>
+
